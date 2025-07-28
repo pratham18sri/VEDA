4997e91942fae28892c7cb31366cac803d048079
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2025-07-28 16:14:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://veda-1-h0ku.onrender.com",
+    origin:"http://loalhost:5173",
     credentials:true
 }))
 const port=process.env.PORT || 5000
