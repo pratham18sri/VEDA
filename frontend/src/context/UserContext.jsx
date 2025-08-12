ec5316646c0318eb2137a19241205d31483c8420
PRATHAM SRIVASTAV <139158707+pratham18sri@users.noreply.github.com>
2025-08-12 23:11:50 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
 export const userDataContext=createContext()
 function UserContext({children}) {
-    const serverUrl="https://veda-piwz.onrender.com"
+    const serverUrl="https://veda-2-o.onrender.com"
     const [userData,setUserData]=useState(null)
     const [frontendImage,setFrontendImage]=useState(null)
      const [backendImage,setBackendImage]=useState(null)
